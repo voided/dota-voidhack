200529e376e062a60375c30174379e57c8b82696
Ryan Stecker <voidedweasel@gmail.com>
2013-10-13 02:39:17 -0500

    Refactor how ability script data is exposed.

--- vhLib/dota/scriptmanager.cpp
+++ vhLib/dota/scriptmanager.cpp
@@ -17,23 +17,8 @@ CScriptManager &ScriptManager()
 
 void CScriptManager::Init()
 {
-	Assert( g_pFullFileSystem );
-
-	m_pKvItems = new KeyValues( "" );
-	if ( !m_pKvItems->LoadFromFile( g_pFullFileSystem, "scripts/npc/items.txt" ) )
-	{
-		Warning( "[ScriptManager] Unable to load item data!\n" );
-		m_pKvItems->deleteThis();
-		m_pKvItems = NULL;
-	}
-
-	m_pKvAbilities = new KeyValues( "" );
-	if ( !m_pKvAbilities->LoadFromFile( g_pFullFileSystem, "scripts/npc/npc_abilities.txt" ) )
-	{
-		Warning( "[ScriptManager] Unable to load ability data!\n" );
-		m_pKvAbilities->deleteThis();
-		m_pKvAbilities = NULL; 
-	}
+	m_pKvItems = LoadScript( "scripts/npc/items.txt" );
+	m_pKvAbilities = LoadScript( "scripts/npc/npc_abilities.txt" );
 }
 
 
@@ -50,6 +35,36 @@ void CScriptManager::Shutdown()
 		m_pKvAbilities->deleteThis();
 		m_pKvAbilities = NULL;
 	}
+
+	FOR_EACH_MAP_FAST( m_AbilityMap, i )
+	{
+		delete m_AbilityMap[ i ];
+	}
+}
+
+KeyValues *CScriptManager::LoadScript( const char *scriptName )
+{
+	Assert( g_pFullFileSystem );
+
+	KeyValues *pKvScript = new KeyValues( "" );
+	if ( !pKvScript->LoadFromFile( g_pFullFileSystem, scriptName ) )
+	{
+		Warning( "[ScriptManager] Unable to load script: %s\n", scriptName );
+
+		pKvScript->deleteThis();
+		return NULL;
+	}
+
+	int version = pKvScript->GetInt( "Version" );
+	if ( version != 1 )
+	{
+		Warning( "[ScriptManager] Version mismatch for script '%s' expecting 1, got %d", scriptName, version );
+
+		pKvScript->deleteThis();
+		return NULL;
+	}
+
+	return pKvScript;
 }
 
 
@@ -61,15 +76,60 @@ KeyValues *CScriptManager::GetItemInfo( const char *itemName )
 	return m_pKvItems->FindKey( itemName );
 }
 
-KeyValues *CScriptManager::GetAbilityInfo( const char *abilityName )
+DOTAAbilityInfo_t *CScriptManager::GetAbilityInfo( const char *abilityName )
 {
 	if ( !m_pKvAbilities )
 		return NULL;
 
-	KeyValues *pBaseAbility = m_pKvAbilities->FindKey( "ability_base" );
+	auto abilityIndex = m_AbilityMap.Find( abilityName );
+
+	if ( m_AbilityMap.IsValidIndex( abilityIndex ))
+	{
+		// return the cached version
+		return m_AbilityMap[ abilityIndex ];
+	}
+
 	KeyValues *pAbility = m_pKvAbilities->FindKey( abilityName );
 
-	pAbility->MergeFrom( pBaseAbility );
+	if ( !pAbility )
+		return NULL;
 
-	return pAbility;
+	KeyValues *pBaseAbility = m_pKvAbilities->FindKey( "ability_base" );
+	Assert( pBaseAbility );
+
+	// create a temp copy so we don't inflate the memory usage of every ability
+	pAbility = pAbility->MakeCopy();
+	KeyValuesAD abilityAutoDelete( pAbility );
+
+	// merge in the base ability data
+	pAbility->RecursiveMergeKeyValues( pBaseAbility );
+
+	DOTAAbilityInfo_t *pInfo = new DOTAAbilityInfo_t;
+
+	pInfo->id = pAbility->GetInt( "ID" );
+
+	FOR_EACH_SUBKEY( pAbility->FindKey( "AbilitySpecial" ), pSpecialKey )
+	{
+		DOTAAbilitySpecial_t *pSpecial = new DOTAAbilitySpecial_t;
+
+		// first subkey is the field type
+		KeyValues *pSub = pSpecialKey->GetFirstSubKey();
+
+		const char *specialType = pSub->GetString();
+
+		// the rest is the field type and values
+		pSub = pSub->GetNextKey();
+
+		const char *specialName = strdup( pSub->GetName() ); // copy so we don't try to free keyvaluesystem's memory on shutdown
+		const char *specialValue = pSub->GetString();
+
+		pSpecial->type = specialType;
+		V_SplitString( specialValue, " ", pSpecial->values );
+
+		pInfo->abilitySpecial.Insert( specialName, pSpecial );
+	}
+
+	m_AbilityMap.Insert( abilityName, pInfo );
+
+	return pInfo;
 }
--- vhLib/dota/scriptmanager.h
+++ vhLib/dota/scriptmanager.h
@@ -3,8 +3,58 @@
 
 #include <cstddef>
 
+#include "utlmap.h"
+#include "utlstring.h"
+#include "utlvector.h"
+
 
 class KeyValues;
+
+
+struct DOTAAbilitySpecial_t
+{
+	~DOTAAbilitySpecial_t()
+	{
+		// todo: we ~should~ be purging and deleting here because of the V_SplitString usage
+		// but since we don't have IMemAlloc, the container will attempt to delete from the wrong heap
+		// values.PurgeAndDeleteElements();
+	}
+
+	CUtlString type;
+	CUtlVector<char *> values;
+};
+
+struct DOTAAbilityInfo_t
+{
+	DOTAAbilityInfo_t()
+		: abilitySpecial( CaselessStringLessThan )
+	{
+	}
+
+	~DOTAAbilityInfo_t()
+	{
+		FOR_EACH_MAP_FAST( abilitySpecial, i )
+		{
+			delete abilitySpecial[ i ];
+		}
+	}
+
+
+	DOTAAbilitySpecial_t *GetSpecialByName( const char *name )
+	{
+		auto specialIndex = abilitySpecial.Find( name );
+
+		if ( !abilitySpecial.IsValidIndex( specialIndex ) )
+			return NULL;
+
+		return abilitySpecial[ specialIndex ];
+	}
+
+
+	int id;
+
+	CUtlMap<const char *, DOTAAbilitySpecial_t *> abilitySpecial;
+};
 
 
 class CScriptManager
@@ -12,7 +62,8 @@ class CScriptManager
 
 public:
 	CScriptManager() :
-		m_pKvItems( NULL ), m_pKvAbilities( NULL )
+		m_pKvItems( NULL ), m_pKvAbilities( NULL ),
+		m_AbilityMap( CaselessStringLessThan )
 	{
 	}
 
@@ -22,12 +73,17 @@ public:
 
 
 	KeyValues *GetItemInfo( const char *itemName );
-	KeyValues *GetAbilityInfo( const char *abilityName );
+	DOTAAbilityInfo_t *GetAbilityInfo( const char *abilityName );
+
+private:
+	KeyValues *LoadScript( const char *scriptName );
 
 
 private:
 	KeyValues *m_pKvItems;
 	KeyValues *m_pKvAbilities;
+
+	CUtlMap<const char *, DOTAAbilityInfo_t *> m_AbilityMap;
 
 };
 
